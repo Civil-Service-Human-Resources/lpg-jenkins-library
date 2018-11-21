48161197b517fcbadb99025eca84d97adff909c3
Rob Marks <robertm@kainos.com>
2018-11-21 17:49:16 +0000

    update tfdeploy.groovy

--- vars/tfdeploy.groovy
+++ vars/tfdeploy.groovy
@@ -1,10 +1,10 @@
-def call(def terraformModuleName, def terraformVarTag) {
+def call(def terraformModuleName, def terraformVarTag, def environment) {
     sh "set +e; rm -rf lpg-terraform-paas"
     sh "git clone https://github.com/Civil-Service-Human-Resources/lpg-terraform-paas.git"
     dir("lpg-terraform-paas/environments/master") {
-        sh "ln -s ${SF}/azure/cabinet-azure/00-integration/state.tf state.tf"
-        sh "ln -s ${SF}/azure/cabinet-azure/00-integration/integration-vars.tf integration-vars.tf"
-        sh "ln -s ../00-integration/00-vars.tf 00-vars.tf"
+        sh "ln -s ${SF}/azure/cabinet-azure/00-${environment}/state.tf state.tf"
+        sh "ln -s ${SF}/azure/cabinet-azure/00-${environment}/integration-vars.tf integration-vars.tf"
+        sh "ln -s ../00-${environment}/00-vars.tf 00-vars.tf"
         sh "terraform --version"
         sh "terraform init"
         sh "terraform validate"
