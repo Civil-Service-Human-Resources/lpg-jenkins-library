7d1cfcacaac8bce6899f8684dbbc62081b69748c
jaylco <jamie.lovell@cabinetoffice.gov.uk>
2021-02-16 13:23:19 +0000

    added build ID back in and fixed push

--- vars/nodeIntegrationPipeline.groovy
+++ vars/nodeIntegrationPipeline.groovy
@@ -47,9 +47,10 @@ def call(body) {
                     script {
                         docker.withRegistry("${env.DOCKER_REGISTRY_URL}", 'docker_registry_credentials') {
                             def now = new Date()
-                            def formatted = now.format("ddMMyy-HHmmss")
-                            def customImage = docker.build(pipelineParams.dockerRepository+":${env.BRANCH_NAME}-${formatted}")
-                            customImage.push("${env.BRANCH_NAME}-${now}")
+                            def formatted = now.format("yyyyMMdd-HHmmss")
+                            def buildId = "${env.BRANCH_NAME}-${env.BUILD_ID}-${formatted}"
+                            def customImage = docker.build(pipelineParams.dockerRepository+":${buildId}")
+                            customImage.push("${buildId}")
                         }
                     }
                 }
