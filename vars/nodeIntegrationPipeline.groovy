2c3e3086875192def5ca4deb16f3a65f837cd18a
Jamie <jamie.lovell@cabinetoffice.gov.uk>
2021-11-10 16:34:09 +0000

    new imaging naming convention

--- vars/nodeIntegrationPipeline.groovy
+++ vars/nodeIntegrationPipeline.groovy
@@ -46,11 +46,13 @@ def call(body) {
                     unstash 'workspace'
                     script {
                         docker.withRegistry("${env.DOCKER_REGISTRY_URL}", 'docker_registry_credentials') {
-                            def now = new Date()
-                            def formattedCurrentDateTime = now.format("yyyyMMdd-HHmmss")
-                            def buildId = "${env.BRANCH_NAME}-${env.BUILD_ID}-${formattedCurrentDateTime}"
-                            def customImage = docker.build(pipelineParams.dockerRepository+":${buildId}")
-                            customImage.push("${buildId}")
+                            def acrRegionName = "test"
+                            if (env.BRANCH_NAME =~ '^v\d{1,2}(\.\d{1,2}){2}$') {
+                                acrRegionName = "prod"
+                            }
+                            def acrRepoName =  + "${pipelineParams.dockerRepository}/${acrRegionName}"
+                            def customImage = docker.build("${acrRepoName}:${env.BRANCH_NAME}")
+                            customImage.push("${env.BRANCH_NAME}")
                         }
                     }
                 }
