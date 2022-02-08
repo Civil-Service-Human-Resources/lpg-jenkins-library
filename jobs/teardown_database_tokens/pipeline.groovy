068d872b19045e2674e691fe50582890102b1269
Jamie <jamie.lovell@cabinetoffice.gov.uk>
2022-02-08 21:37:08 +0000

    correct usage of credentials

--- jobs/teardown_database_tokens/pipeline.groovy
+++ jobs/teardown_database_tokens/pipeline.groovy
@@ -5,19 +5,19 @@ pipeline {
         skipDefaultCheckout()
     }
 
+    environment {
+        SQL_HOST = credentials('prod_mysql_host')
+        SQL_USER = credentials('prod_mysql_gp_user')
+        SQL_PASSWORD = credentials('Prod_MySQL_GP_Pass')
+    }
+
     stages {
         stage("Clear tokens") {
             agent { label 'master' }
             steps {
-                withCredentials([
-                    string(crdentialsId: 'prod_mysql_host', variable: 'SQL_HOST'),
-                    string(crdentialsId: 'prod_mysql_gp_user', variable: 'SQL_USER'),
-                    string(crdentialsId: 'Prod_MySQL_GP_Pass', variable: 'SQL_PASSWORD')
-                ]) {
-                    script {
-                        sh 'python3 -m pip install -r requirements.txt'
-                        sh 'python3 script.py'
-                    }
+                script {
+                    sh 'python3 -m pip install -r requirements.txt'
+                    sh 'python3 script.py'
                 }
             }
         }
