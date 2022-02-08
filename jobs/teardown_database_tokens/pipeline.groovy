8f9f8e35a6f02ec8edd22717dadf52c6c860f8d8
Jamie <jamie.lovell@cabinetoffice.gov.uk>
2022-02-08 21:44:23 +0000

    correct way to checkout branch

--- jobs/teardown_database_tokens/pipeline.groovy
+++ jobs/teardown_database_tokens/pipeline.groovy
@@ -14,10 +14,18 @@ pipeline {
     stages {
         stage("Clear tokens") {
             agent { label 'master' }
+
             steps {
-                script {
-                    sh 'python3 -m pip install -r requirements.txt'
-                    sh 'python3 script.py'
+                checkout([
+                    $class: 'GitSCM',
+                    branches: [[name: 'idt-feature-LC-1113-duplicate-token-cleanup']],
+                    userRemoteConfigs: [[url: 'https://github.com/Civil-Service-Human-Resources/lpg-jenkins-library']]
+                ])
+                dir('./jobs/teardown_database_tokens') {
+                    script {
+                        sh 'python3 -m pip install -r requirements.txt'
+                        sh 'python3 script.py'
+                    }
                 }
             }
         }
